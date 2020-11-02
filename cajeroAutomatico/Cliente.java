f47eaaddda2ada182b0d70379beca1104248ae5b
jdpc-hub <jdpc20392@gmail.com>
2020-11-02 12:04:04 -0500

    Se corrige la sangría de los archivos.

--- cajeroAutomatico/Cliente.java
+++ cajeroAutomatico/Cliente.java
@@ -2,35 +2,35 @@ package cajeroAutomatico;
 
 class Cliente {
 
-	//Variables de instancia
-	private String cedula;
-	private String nombre;
-	private double saldo;
+    //Variables de instancia
+    private String cedula;
+    private String nombre;
+    private double saldo;
 
-	//Constructor
-	Cliente(String cedula, String nombre, double saldo) {
-		this.cedula = cedula;
-		this.nombre = nombre;
-		this.saldo = saldo;
-	}
+    //Constructor
+    Cliente(String cedula, String nombre, double saldo) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.saldo = saldo;
+    }
 
-	//Métodos
-	public String getCedula() {
-		return cedula;
-	}
-	public void setCedula(String cedula) {
-		this.cedula = cedula;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public double getSaldo() {
-		return saldo;
-	}
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
+    //Métodos
+    public String getCedula() {
+        return cedula;
+    }
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 }
